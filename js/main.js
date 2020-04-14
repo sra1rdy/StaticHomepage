46fed33554dd2fa3cbe79acaf155cb7c3a7adb3f
Sravan Reddy <sravanreddy235@gmail.com>
2020-04-14 15:34:17 +0200

    script for consent banner policy added

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const cookieConsent  = document.getElementById('cookieConsent');
+const cookieConsentBtn = document.getElementById('cookieBtn');
+
+const elMenu = document.getElementById('mainMenu');
+const menuBtn = document.getElementById('menuBtn');
+
+const consentDataBrowser = localStorage.getItem('cookieBannerDisplayed');
+
+menuBtn.addEventListener('click', toggleClass);
+cookieConsentBtn.addEventListener('click', handleClick, {once: true});
+
+
+
+
+
+function toggleClass(){
+   
+ 
+   elMenu.classList.toggle('show');
+}
+
+
+function handleClick () {
+// remove the consent bar
+    cookieConsent.classList.add('active');
+    localStorage.setItem("cookieBannerDisplayed", "true");
+}
+
+/**
+ * If the user agreed to the consent policy by clicking 'einversanden' when he/she visited the website first time, 
+we save the user decision in the local storage of the browser. 
+ Hence, we do not display the consent on return of the user to the homepage.**/
+(function () {
+    if (consentDataBrowser) {
+        cookieConsent.classList.add('active');
+    }
+  })();
