d7e5079be0d182ad3bda5e21f3446c0d7b453f84
Sravan Reddy <sravanreddy235@gmail.com>
2020-04-16 09:31:40 +0200

    nav overlap on mobile devices fix

--- js/main.js
+++ js/main.js
@@ -21,14 +21,13 @@ function toggleClass(){
 
 
 function handleClick () {
-// remove the consent bar
     cookieConsent.classList.add('active');
     localStorage.setItem("cookieBannerDisplayed", "true");
 }
 
 /**
- * If the user agreed to the consent policy by clicking 'einversanden' when he/she visited the website first time, 
-we save the user decision in the local storage of the browser. 
+ * If the user agreed to the consent policy by clicking 'einverstanden' when he/she visited the website first time, 
+we save the user decision in the local storage of the user's browser. 
  Hence, we do not display the consent on return of the user to the homepage.**/
 (function () {
     if (consentDataBrowser) {
