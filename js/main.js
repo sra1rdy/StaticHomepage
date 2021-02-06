e7d044e4d7773e565e779d90313c5e283fd6e2bf
Sravan Reddy Jeedipelly <sravanreddy235@gmail.com>
2021-02-06 13:19:05 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,26 +9,18 @@ const consentDataBrowser = localStorage.getItem('cookieBannerDisplayed');
 menuBtn.addEventListener('click', toggleClass);
 cookieConsentBtn.addEventListener('click', handleClick, {once: true});
 
-
-
-
-
 function toggleClass(){
-   
- 
-   elMenu.classList.toggle('show');
+    elMenu.classList.toggle('show');
 }
-
 
 function handleClick () {
     cookieConsent.classList.add('active');
     localStorage.setItem("cookieBannerDisplayed", "true");
 }
 
-/**
- * If the user agreed to the consent policy by clicking 'einverstanden' when he/she visited the website first time, 
-we save the user decision in the local storage of the user's browser. 
- Hence, we do not display the consent on return of the user to the homepage.**/
+/* If the user agreed to the consent policy by clicking 'einverstanden' when he/she visited the website first time, 
+   we save the user decision in the local storage of the user's browser. 
+   Hence, we do not display the consent on return of the user to the homepage. */
 (function () {
     if (consentDataBrowser) {
         cookieConsent.classList.add('active');
